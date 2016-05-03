ef56649b23588a57c3710d51324733418c30ca3a
voidz777 <voidz.evo3d@gmail.com>
2016-05-03 17:07:43 +0900

    celoxdcm: DU-ify!

--- du.mk
+++ du.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/celoxdcm/full_celoxdcm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Override AOSP build properties
 PRODUCT_BRAND := samsung
@@ -13,6 +13,6 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SC-03D TARGET_DEVICE=SC-03D BUILD_FINGERPRINT="samsung/SC-03D/SC-03D:6.0/LMY47I/OMPP5:user/release-keys" PRIVATE_BUILD_DESC="SC-03D-user 6.0 LMY47I OMMP5 release-keys"
 
 
-PRODUCT_NAME := cm_celoxdcm
+PRODUCT_NAME := du_celoxdcm
 PRODUCT_DEVICE := celoxdcm
 
